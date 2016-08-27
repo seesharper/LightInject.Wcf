1d1e2b727f2797dd1e3f74eb3ab35e1b73225cfd
Geir Sagberg <geir.sagberg@gmail.com>
2016-08-27 16:04:05 +0200

    Updated for 4.1.0

--- src/LightInject.Wcf/LightInject.Wcf.cs
+++ src/LightInject.Wcf/LightInject.Wcf.cs
@@ -21,7 +21,7 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 ******************************************************************************
-   LightInject.Wcf version 1.0.0.1 
+   LightInject.Wcf version 1.1.0 
    http://www.lightinject.net/
    http://twitter.com/bernhardrichter
 ******************************************************************************/
